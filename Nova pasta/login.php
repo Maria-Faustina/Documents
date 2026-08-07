bec129877a365f9be17f87aed025792c0de53031
Maria Clara Faustina <mariaclarafsp299@gmail.com>
2026-08-07 12:12:12 -0300

    Add files via upload

---
+++ Nova pasta/login.php
@@ -0,0 +1,33 @@
+<?php
+    session_start(); 
+    require 'conexao.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = trim($_POST['email']);
+        $senha = $_POST['senha']; 
+
+        try {
+            $stmt = $pdo->prepare("SELECT id, password, email FROM cadastro WHERE email = :email");
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
+            
+            $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            if ($usuario) {
+                if ($senha === $usuario['password']) {
+                    $_SESSION['usuario_id'] = $usuario['id'];
+                    $_SESSION['usuario_email'] = $usuario['email'];
+                    
+                    header("Location: index.php");
+                    exit;
+                } else {
+                    echo "<script>alert('A Senha está incorreta!'); window.location.href='login.html';</script>";
+                }
+            } else {
+                echo "<script>alert('Este email não está registado!'); window.location.href='login.html';</script>";
+            }
+        } catch (PDOException $e) {
+            echo "Erro no login: " . $e->getMessage();
+        }
+    }
+?>
